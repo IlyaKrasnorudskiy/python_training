677bbc02719e57fba92f99ead09512b091618811
Ilya <fgf625@mail.ru>
2022-08-03 18:55:28 +0300

    3.08.22

--- train by codewars.py
+++ train by codewars.py
@@ -70,3 +70,116 @@ def sum_array(arr):
 def repeat_str(repeat, string):
     return string*repeat
 
+num = 0
+def descending_order(num):
+    while True:
+        if num == 0:
+            return num
+        num = str(num)
+        arr = []
+        stroka = ''
+        for i in num:
+            arr.append(int(i))
+        arr.sort()
+        for i in range(-1, -len(arr)-1, -1):
+            stroka += str(arr[i])
+        stroka = int(stroka)
+        break
+    return stroka
+print(descending_order(151))
+s = ['2','2','1']
+s = ''.join(s)
+print(int(s)+1)
+
+a = 6
+b = 2
+c = bin(a+b)
+
+
+def add_binary(a, b):
+    x = a + b
+    s = ''
+    while x != 0:
+        s += str(x % 2)
+        x = x // 2
+    return s[::-1]
+print(2.5/100)
+
+def nb_year(p0, percent, aug, p):
+    k = 0
+    while p0 < p:
+        p0 = p0 + p0*percent/100 + aug
+        k+=1
+    return k
+
+def accum(s):
+    new_str = ''
+    for i in range(0, len(s)):
+        new_str += s[i].capitalize()
+        new_str += s[i].lower() * i
+        if i != len(s)-1:
+            new_str += '-'
+    return new_str
+print(accum('ZFasdad'))
+a = '+'
+val1= '1'
+val2= '2'
+str = str(val1)+a+str(val2)
+print(str)
+
+
+
+
+def century(year):
+    if year%10>0 or year//10%10>0:
+        return year//100 + 1
+    else:
+        return year//100
+
+a=3
+b=2
+if type(a) == type(b):
+    print('good')
+
+
+def filter_list(l):
+    help = 2
+    for a in l:
+        if type(a) != type(help):
+            l.remove(a)
+    return l
+
+print(filter_list([1, 2,'b','c']))
+
+
+#МУТАЦИЯ
+def remove_smallest(numbers):
+    min = 100000000
+    member = 0
+    for i in range(0, len(numbers)):
+        if min > numbers[i]:
+            min = numbers[i]
+            member = i
+    if len(numbers) > 0:
+        numbers.pop(member)
+    return numbers
+
+def find_average(numbers):
+    sum = 0
+    for a in numbers:
+        sum+=a
+    return sum/len(numbers)
+
+#
+def high_and_low(numbers):
+    max = 0
+    min = 9999
+    for i in numbers:
+        if i != ' ' or i != '-':
+            if min > int(i):
+                min = int(i)
+            if max < int(i):
+                max = int(i)
+    string = str(max) + ' ' + str(min)
+    return string
+
