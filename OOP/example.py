662e3885338fdf2def8af5ba5ec978de6a4a1038
Ilya <105754590+IlyaKrasnorudskiy@users.noreply.github.com>
2023-04-04 21:59:25 +0300

    04.04.23

---
+++ OOP/example.py
@@ -0,0 +1,42 @@
+# Базовый класс Vehicle (транспортное средство)
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        return "Двигатель запущен"
+
+    def stop_engine(self):
+        return "Двигатель остановлен"
+
+
+# Класс Car наследует от класса Vehicle
+class Car(Vehicle):
+    def __init__(self, make, model, year, num_doors):
+        super().__init__(make, model, year)
+        self.num_doors = num_doors
+
+    def honk(self):
+        return "Би-би!"
+
+
+# Класс Motorcycle наследует от класса Vehicle
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, year, has_sidecar):
+        super().__init__(make, model, year)
+        self.has_sidecar = has_sidecar
+
+    def honk(self):
+        return "Гудок мотоцикла!"
+
+
+
+car = Car("Toyota", "Camry", 2020, 4)
+motorcycle = Motorcycle("Harley-Davidson", "Roadster", 2019, False)
+
+print(car.start_engine())
+print(car.honk())
+print(motorcycle.start_engine())
+print(motorcycle.honk())
