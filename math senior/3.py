3d10cde0f2b56010b8f2062840c3e211389cd607
Ilya <105754590+IlyaKrasnorudskiy@users.noreply.github.com>
2025-04-03 15:17:13 +0300

    03 04 25

--- math senior/3.py
+++ math senior/3.py
@@ -1,1 +1,9 @@
-
+count = 0
+for x1 in range(0,2):
+    for x2 in range(0, 2):
+        for x3 in range(0, 2):
+            for x4 in range(0, 2):
+                if (((x1 > x2)) <= x3) == 1 and (((x2 > x3)) <= x4) == 1:
+                    count += 1
+                    print(x1,x2,x3,x4)
+print(count)
